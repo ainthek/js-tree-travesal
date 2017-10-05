8232d3decacd2045059c75e1b9e0bf8700ea32fb
Marko Martin <marcus@gratex.com>
2017-10-06 01:03:33 +0200

    wip

---
+++ src/delegate.js
@@ -0,0 +1,47 @@
+// just inspiration, simplified not fully correct
+module.exports = (function() {
+    // boodman/crockford delegation w/ cornford optimization
+
+    /*=====
+		delegate: function(obj, props){
+			// summary:
+			//		Returns a new object which "looks" to obj for properties which it
+			//		does not have a value for. Optionally takes a bag of properties to
+			//		seed the returned object with initially.
+			// description:
+			//		This is a small implementation of the Boodman/Crockford delegation
+			//		pattern in JavaScript. An intermediate object constructor mediates
+			//		the prototype chain for the returned object, using it to delegate
+			//		down to obj for property lookup when object-local lookup fails.
+			//		This can be thought of similarly to ES4's "wrap", save that it does
+			//		not act on types but rather on pure objects.
+			// obj: Object
+			//		The object to delegate to for properties not found directly on the
+			//		return object or in props.
+			// props: Object...
+			//		an object containing properties to assign to the returned object
+			// returns:
+			//		an Object of anonymous type
+			// example:
+			//	|	var foo = { bar: "baz" };
+			//	|	var thinger = lang.delegate(foo, { thud: "xyzzy"});
+			//	|	thinger.bar == "baz"; // delegated to foo
+			//	|	foo.thud == undefined; // by definition
+			//	|	thinger.thud == "xyzzy"; // mixed in from props
+			//	|	foo.bar = "thonk";
+			//	|	thinger.bar == "thonk"; // still delegated to foo's bar
+		},
+		=====*/
+
+    function TMP() {}
+    return function(obj, props) {
+        TMP.prototype = obj;
+        var tmp = new TMP();
+        TMP.prototype = null;
+        if (props) {
+            Object.assign(tmp, props);
+        }
+        console.log("returning", tmp);
+        return tmp; // Object
+    };
+})();
--- src/revivers.js
+++ src/revivers.js
@@ -1,8 +1,8 @@
-// just inspiration, simplified not fully correct
 
 module.exports = {
     valueof,
-    parent
+    parent,
+    mixin
 }
 
 function self(v) {
@@ -21,14 +21,35 @@ function valueof(k, v) {
 }
 
 function parent(k, v) {
-	
+
     if (typeof v !== 'object') {
         return {
             valueOf: self(v)
         }
     } else {
-    	
+
         Object.keys(v).forEach((p) => v[p]._parent = v);
         return v;
     }
 }
+
+function mixin(k, v) {
+    // var a = {a: 1}; 
+    // // a ---> Object.prototype ---> null
+
+    // var b = Object.create(a);
+    // // b ---> a ---> Object.prototype ---> null
+    // console.log(b.a); // 1 (inherited)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+    if (typeof v !== 'object') {
+        return v;
+    } else {
+        Object.keys(v).forEach((p) => {
+            if (typeof v[p] == 'object') {
+                v[p] =_d(v, v[p]);
+            }
+        });
+        return v;
+    }
+}
+
